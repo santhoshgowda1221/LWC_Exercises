d7ee5e1c62380fb013e85aa9b252eed0c4a652a4
Santhosh <santhoshgowda@live.com>
2021-11-18 12:25:26 +0530

    excerice 2-8

--- force-app/Exercises/main/default/lwc/studentBrowserForm/studentBrowserForm.js
+++ force-app/Exercises/main/default/lwc/studentBrowserForm/studentBrowserForm.js
@@ -1,3 +1,75 @@
-import { LightningElement } from 'lwc';
+import {
+    LightningElement,
+    wire
+} from 'lwc';
+import getInstructors from "@salesforce/apex/studentBrowserForm.getInstructors";
+import getDeliveryInstructorId from "@salesforce/apex/studentBrowserForm.getDeliveryInstructorId";
+import {NavigationMixin} from 'lightning/navigation';
+import { encodeDefaultFieldValues } from 'lightning/pageReferenceUtils';
+export default class StudentBrowserForm extends NavigationMixin(LightningElement) {
+    selectedInstructorId = '';
+    selectedDeliveryId = '';
+    instructors = [];
+    deliveries = [];
+    error;
 
-export default class StudentBrowserForm extends LightningElement {}
+    @wire(getInstructors) wiredInstructors({error,data}) {
+        this.instructors = [];
+        if (data) {
+            this.instructors.push({
+                value: '',
+                label: 'select an instructor'
+            });
+            data.forEach(instructor => {
+                this.instructors.push({
+                    value:instructor.Id,
+                    label:instructor.Name
+                });
+            })
+        } else if (error) {
+            this.error = error;
+        }
+    }
+
+    @wire(getDeliveryInstructorId,{instructorId:'$selectedInstructorId'}) wired_getDeliveriesByInstructor({ error, data }) {
+        this.deliveries = [];
+        if (data && data.length) {
+            
+            this.deliveries = data.map(delivery => ({
+                value: delivery.Id,
+                label: `${delivery.Start_Date__c} ${delivery.Location__c} ${delivery.Attendee_Count__c} students`
+            }));
+    
+            this.deliveries.unshift({
+                value: '',
+                label: 'Any Delivery' 
+            });
+    
+        } else if (error) {
+            this.error = error;
+        }
+    }
+    onInstructorChange(event){
+        this.selectedInstructorId = event.target.value;
+    }
+    onDeliveryChange(event){
+        this.selectedDeliveryId = event.target.value
+    }
+
+    createNewDelivery() {
+        const defaultValues = encodeDefaultFieldValues({
+            Instructor__c: this.selectedInstructorId
+        });
+
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Course_Delivery__c',
+                actionName: 'new'
+            },
+            state: {
+                defaultFieldValues: defaultValues
+            }
+        });
+    }
+}
