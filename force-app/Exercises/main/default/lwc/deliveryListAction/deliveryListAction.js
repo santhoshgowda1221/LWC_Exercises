e6f0f6cd057ba665d3a3245c982b92eb05c0c973
Santhosh <santhoshgowda@live.com>
2021-11-19 13:49:55 +0530

    Excerice 3-2 completed

--- force-app/Exercises/main/default/lwc/deliveryListAction/deliveryListAction.js
+++ force-app/Exercises/main/default/lwc/deliveryListAction/deliveryListAction.js
@@ -1,3 +1,5 @@
 import { LightningElement } from 'lwc';
 
-export default class DeliveryListAction extends LightningElement {}
+export default class DeliveryListAction extends LightningElement {
+    
+}
